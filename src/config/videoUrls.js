c586c77297623462a944d56733afe601a5199216
Yoshita Sharma <yoshita.as.sharma@gmail.com>
2026-03-15 09:47:25 -0700

    Fix 'Menstrual Cycle' video link

--- src/config/videoUrls.js
+++ src/config/videoUrls.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
 export const videoUrls = {
   intro: '1773592378673-a052n43319v.mp4',
-  cycle: 'understanding_the_menstrual_cycle.mp4',
+  cycle: '1773592833285-ax2boy14rg.mp4',
   products: null,
   myths: null,
   hygiene: null,
